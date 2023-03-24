6c245e476bce59aa100b9f6bdd74d5bd80c4bb50
Dozie87 <dozie.kingsley20@gmail.com>
2023-03-24 01:45:22 -0400

    Project 2 completed

--- Resources/crowdfunding_db_schema.sql
+++ Resources/crowdfunding_db_schema.sql
@@ -1,20 +1,3 @@
-Create table campaign(
-	cf_id int not null,
-	contact_id int not null,
-	company_name varchar(100) not null,
-	description TEXT not null,
-	goal numeric (10,2) not null,
-	pledged numeric (10,2) not null,
-	outcome varchar(50) not null,
-	backers_count int not null,
-	country varchar(10)not null,
-	currency varchar(10) not null,
-	launched_date DATE not null,
-	end_date DATE not null,
-	category_id varchar(10) not null,
-	subcategory_id varchar(10) not null
-);
-
 create table category(
 	category_id varchar(10) primary key not null,
 	category_name varchar(50) not null
@@ -32,6 +15,25 @@ create table contacts(
 	email varchar(100) not null
 );
 
+Create table campaign(
+	cf_id int primary key not null,
+	contact_id int not null,
+	company_name varchar(100) not null,
+	description TEXT not null,
+	goal numeric (10,2) not null,
+	pledged numeric (10,2) not null,
+	outcome varchar(50) not null,
+	backers_count int not null,
+	country varchar(10)not null,
+	currency varchar(10) not null,
+	launched_date DATE not null,
+	end_date DATE not null,
+	category_id varchar(10) not null,
+	subcategory_id varchar(10) not null,
+	foreign key(category_id ) references category(category_id),
+	foreign key(subcategory_id ) references subcategory(subcategory_id),
+	foreign key(contact_id ) references contacts(contact_id)
+);
 
 select * from campaign
 select * from category
