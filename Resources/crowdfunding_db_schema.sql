a609f0d14b8047fca58ae73c49b3f6343d7ce7bf
Dozie87 <dozie.kingsley20@gmail.com>
2023-03-24 01:17:44 -0400

    finishing up my project

---
+++ Resources/crowdfunding_db_schema.sql
@@ -0,0 +1,39 @@
+Create table campaign(
+	cf_id int not null,
+	contact_id int not null,
+	company_name varchar(100) not null,
+	description TEXT not null,
+	goal numeric (10,2) not null,
+	pledged numeric (10,2) not null,
+	outcome varchar(50) not null,
+	backers_count int not null,
+	country varchar(10)not null,
+	currency varchar(10) not null,
+	launched_date DATE not null,
+	end_date DATE not null,
+	category_id varchar(10) not null,
+	subcategory_id varchar(10) not null
+);
+
+create table category(
+	category_id varchar(10) primary key not null,
+	category_name varchar(50) not null
+);
+
+create table subcategory(
+	subcategory_id varchar(10) primary key not null,
+	subcategory_name varchar(50) not null
+);
+
+create table contacts(
+	contact_id int primary Key not null,
+	first_name varchar(50) not null,
+	last_name varchar(50) not null,
+	email varchar(100) not null
+);
+
+
+select * from campaign
+select * from category
+select * from subcategory
+select * from contacts
